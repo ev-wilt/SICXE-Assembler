7862f4d603494ae9d46cf0ba480ec99482976f9c
Evan Wilt <ev.wilt@gmail.com>
2018-03-11 17:42:29 -0400

    Fixed parsing so comments won't be included

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
         throw std::invalid_argument("No file was specified.");
     }
     else {
-        std::string token;
+        std::string currentLine = "";
+        std::string token = "";
         std::ifstream file(argv[1]);
         if (file) {
-            while (getline(file, token, '\t')) {
-                if (token[0] != '.' && token != "") {
-                    std::cout << token << std::endl;
+            while (getline(file, currentLine, '\n')) {
+                if (currentLine[0] != '.' && currentLine != "") {
+                    std::stringstream lineStream(currentLine);
+                    while (getline(lineStream, token, '\t')) {
+                        if (token[0] != '.' && token != "") {
+                            std::cout << token << std::endl;
+                        }
+                    }
                 }
             }
         }
