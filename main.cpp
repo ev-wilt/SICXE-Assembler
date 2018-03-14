42e4cf3f58f14831a0095cb71a5b1425c9fa7013
Evan Wilt <ev.wilt@gmail.com>
2018-03-13 21:50:17 -0400

    Made parser insert input into a vector for easier handling

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
+    std::vector<std::string> inputVector;
+    std::vector<std::string>::iterator iter = inputVector.begin();
+
     if (argc <= 1) {
         throw std::invalid_argument("No file was specified.");
     }
@@ -17,10 +21,13 @@ int main(int argc, char* argv[]) {
                     std::stringstream lineStream(currentLine);
                     while (getline(lineStream, token, '\t')) {
                         if (token[0] != '.') {
-                            std::cout << token << std::endl;
+                            iter = inputVector.insert(iter, token);
                         }
                     }
                 }
+            }
+            for (iter = inputVector.begin(); iter < inputVector.end(); ++iter) {
+                std::cout << *iter << std::endl;
             }
         }
         else {
